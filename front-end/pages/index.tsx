7912e00f0a4063ee834063d4770095e983d7bd3b
MengMassart_UCLL <113979804+Lycanthr0py@users.noreply.github.com>
2025-03-17 00:40:11 +0100

    First steps

---
+++ front-end/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Homepage() {
+
+    return (
+        <>
+            <Head>
+                <title>Homepage</title>
+            </Head>
+
+            <main>
+                <div>
+                    
+                </div>
+            </main>
+        </>
+    )
+}
