0ac2434d13beb682584298a525ad6bf3d8f795e9
MengMassart_UCLL <113979804+Lycanthr0py@users.noreply.github.com>
2025-03-31 17:22:28 +0200

    Create StatisticDisplay.tsx

---
+++ front-end/components/displays/StatisticDisplay.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const StatisticDisplay = () => {
+
+
+    return (
+        <>
+            <div>
+                
+            </div>
+        </>
+    );
+};
+
+export default StatisticDisplay;
