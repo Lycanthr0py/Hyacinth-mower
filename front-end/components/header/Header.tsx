5f8d21e6aa4e6eb82ac35eef0f5925f8813d29dd
MengMassart_UCLL <113979804+Lycanthr0py@users.noreply.github.com>
2025-05-02 15:14:07 +0200

    update header

--- front-end/components/header/Header.tsx
+++ front-end/components/header/Header.tsx
@@ -2,8 +2,24 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <header style={{ backgroundColor: 'green', padding: '1rem' }}>
-      <h1 style={{ color: 'white', textAlign: 'center', margin: 0 }}>
+    <header
+      style={{
+        background: 'radial-gradient(circle,rgb(0, 100, 3),rgb(0, 200, 10))',
+        padding: '1rem',
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+        marginBottom: '1rem',
+      }}
+    >
+      <h1
+        style={{
+          color: 'white',
+          textAlign: 'center',
+          margin: 0,
+          fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
+          fontSize: '2.5rem',
+          letterSpacing: '2px',
+        }}
+      >
         Hyacinth Mower
       </h1>
     </header>
--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -3,19 +3,19 @@ import AllDisplay from "@components/displays/AllDisplay";
 import Header from "@components/header/Header";
 
 const sampleData = [
-    { time: '06:00', distance: 500 },
-    { time: '07:00', distance: 1800 },
+    { time: '06:00', distance: 200 },
+    { time: '07:00', distance: 1000 },
     { time: '08:00', distance: 0 },
-    { time: '09:00', distance: 800 },
-    { time: '10:00', distance: 1500 },
-    { time: '11:00', distance: 1000 },
-    { time: '12:00', distance: 1200 },
-    { time: '13:00', distance: 2000 },
-    { time: '14:00', distance: 2500 },
-    { time: '15:00', distance: 3200 },
+    { time: '09:00', distance: 300 },
+    { time: '10:00', distance: 600 },
+    { time: '11:00', distance: 300 },
+    { time: '12:00', distance: 100 },
+    { time: '13:00', distance: 500 },
+    { time: '14:00', distance: 500 },
+    { time: '15:00', distance: 800 },
     { time: '16:00', distance: 0 },
-    { time: '17:00', distance: 2000 },
-    { time: '18:00', distance: 800 },
+    { time: '17:00', distance: 1000 },
+    { time: '18:00', distance: 500 },
 ];
 
 export default function Homepage() {
@@ -27,13 +27,11 @@ export default function Homepage() {
             </Head>
             <Header/>
 
-            <body className="m-0 p-0 box-border">
-                <main className="flex flex-row items-start gap-8">
-                    <div className="flex flex-row items-start gap-8">
-                        <AllDisplay battery={75} data={sampleData} status={"obstructed"}/>
-                    </div>
-                </main>
-            </body>
+            <main className="flex flex-row items-start gap-8">
+                <div className="flex flex-row items-start gap-8">
+                    <AllDisplay battery={75} data={sampleData} status={"obstructed"}/>
+                </div>
+            </main>
         </>
     )
 }
